87fabf21ea66f96719f0b5fb30408288430663e7
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-05-10 16:23:04 -0300

    💄Base template changes

--- resources/views/CareUS/Layouts/careus-in.blade.php
+++ resources/views/CareUS/Layouts/careus-in.blade.php
@@ -1,35 +1,59 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="no-js">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset=" UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
     <title>@yield('pageTitle')</title>
 
-    <meta name="author" content="name">
-    <meta name="description" content="description here">
-    <meta name="keywords" content="keywords,here">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ secure_asset('favicon.ico') }}">
+    <script type="module">
+        document.documentElement.classList.remove('no-js');
+        document.documentElement.classList.add('js');
+    </script>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;700&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-    <link href="{{ mix('css/careus.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/careus.css') }}" rel="stylesheet" />
+    <link href="{{ mix('css/careus_print.css') }}" rel="stylesheet" media="print" />
 
     @stack('styles')
+
+    <meta name="author" content="{{ '' }}" />
+    <meta name="description" content="{{ '' }}" />
+    <meta name="keywords" content="{{ '' }}" />
+    <meta name="generator" content="Laravel, PHP, VSCode" />
+
+    <meta property="og:title" content="@yield('pageTitle')" />
+    <meta property="og:description" content="{{ '' }}" />
+    <meta property="og:image" content="{{ config('app.url') }}images/helper/image.jpg" />
+    <meta property="og:image:alt" content="Image description" />
+    <meta property="og:locale" content="{{ app()->getLocale() }}" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="og:url" content="{{ Request::url() }}" />
+    <link rel="canonical" href="{{ Request::url() }}" />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="manifest" href="{{ secure_asset('images/careus.webmanifest') }}" />
+    <link rel="icon" href="{{ secure_asset('favicon.ico') }}" />
+    <link rel="icon" href="{{ secure_asset('favicon.svg') }}" type="image/svg+xml" />
+    <link rel="apple-touch-icon" href="{{ config('app.url') }}images/helper/apple-touch-icon.png" />
+    <meta name="theme-color" content="#98C1D9" />
+
+    <meta name="format-detection" content="telephone=no">
 
 </head>
 
 <body class="font-sans text-base antialiased leading-snug tracking-normal bg-palecerulean-500 text-lightcyan-500">
+
+    <noscript>
+        <h2>{{ __('Javascript needed for this website usage.') }}</h2>
+    </noscript>
 
     <div class="flex flex-col min-h-screen lg:flex-row lg:relative">
 
