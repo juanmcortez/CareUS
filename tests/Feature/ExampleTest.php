e3a025c175ef4e772d263c0c26153dd68bfea6b3
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-05-22 17:58:25 -0300

    ✅ Updating tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Users\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -16,6 +17,22 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(302);
+        //$response->assertStatus(302);
+        $response->assertRedirect('/login');
+        $response->assertSessionHasNoErrors();
+    }
+
+    /**
+     * Check if we can access Patient list page
+     *
+     * @return void
+     */
+    public function test_get_patients_list()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/patients/list');
+
+        $response->assertStatus(200);
     }
 }
