a1d671a9be7330a169e48c5a709137c2c94319af
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-05-22 17:12:08 -0300

    📦 Adapting Breeze to current install

--- resources/CareUS/components/layouts/guest.blade.php
+++ resources/CareUS/components/layouts/guest.blade.php
@@ -60,7 +60,7 @@
         <div class="w-full transition-all duration-200 ease-in-out">
 
             <!-- HEADER -->
-            <header class="text-2xl font-medium">
+            <header class="text-2xl font-medium border-b border-gray-300">
                 <h1 class="py-5 mx-10 text-gunmetal-700">
                     <a href="{{ route('dashboard.index') }}">Care<span class="font-bold text-gunmetal-300">US</span></a>
                 </h1>
