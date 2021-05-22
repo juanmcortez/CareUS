e570249c74ee12e6cde867ee755c4594ab697943
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-05-22 17:34:15 -0300

    📦 Adapting Breeze to current install

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
 
             $table->string('first_name');
-            $table->string('middle_name');
+            $table->string('middle_name')->nullable();
             $table->string('last_name');
 
             $table->string('email')->unique();
