06c9b92e1296eb94e9164a4bcb9df3414b8564b6
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-05-23 03:41:24 -0300

    📦 Adapting Breeze to current install

--- resources/CareUS/components/common/alert.blade.php
+++ resources/CareUS/components/common/alert.blade.php
@@ -1,17 +1,17 @@
 <div class="absolute flex flex-row items-center justify-center w-full leading-loose align-middle"
     x-data="{ showalert: true }">
-    <div class="flex flex-row w-1/2 bg-{{ $type }}-300 text-{{ $type }}-800 px-3 py-2 rounded-md transition-opacity ease-in-out duration-300"
-        :class="{ 'opacity-0': !showalert }">
+    <div class="flex flex-row w-1/2 bg-{{ $type }}-300 text-{{ $type }}-800 px-3 py-2 rounded-md"
+        :class="{ 'hidden': !showalert }">
         <div class="flex flex-col flex-wrap items-center justify-start w-11/12">
             @foreach ($message->all() as $error)
-            <p class="flex flex-row items-center justify-start w-full align-middle">
+            <p class="inline-block w-full text-sm leading-loose align-middle">
                 <i class="mr-2 text-{{ $type }}-600 fas fa-exclamation-triangle animate-pulse"></i>
-                {{ $error }}
+                {!! $error !!}
             </p>
             @endforeach
         </div>
         <div class="flex flex-row items-start justify-end w-1/12 cursor-pointer" @click="showalert = false">
-            <span class="sr-only">Close panel</span>
+            <span class="sr-only">{{ __('Close panel') }}</span>
             <i class="mt-2 mr-1 text-xl fas fa-times"></i>
         </div>
     </div>
--- resources/CareUS/components/common/forms/button.blade.php
+++ resources/CareUS/components/common/forms/button.blade.php
@@ -1,3 +1,9 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props([ 'color' => 'blue', 'icon' => 'info-circle' ])
+
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-'.$color.'-500
+    border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+    hover:bg-'.$color.'-600 active:bg-'.$color.'-600 focus:outline-none focus:border-'.$color.'-600 focus:ring
+    ring-'.$color.'-500 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    <i class="mr-2 fas fa-{{ $icon }}"></i>
     {{ $slot }}
 </button>
