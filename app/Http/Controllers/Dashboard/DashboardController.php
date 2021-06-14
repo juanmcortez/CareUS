602bd61d52f6006c2182323bb194fa32218a7a55
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-14 12:31:22 -0300

    🏗 Dashboard first stats

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Common\Persona;
+use App\Models\Patients\Patient;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
@@ -15,6 +18,34 @@ class DashboardController extends Controller
     {
         $pageH2 = __("Dashboard");
         $pageTitle = $pageH2 . ' | ' . config('app.name');
-        return view('Dashboard.index', compact('pageTitle', 'pageH2'));
+
+        // Get last 30 days stats
+        $today = Carbon::now()->format('Y-m-d 23:59:59');
+        $day30 = Carbon::now()->subDays(30)->format('Y-m-d 00:00:00');
+        $d3060 = Carbon::parse(Carbon::now()->subDays(31)->format('Y-m-d 23:59:59'))->subDays(30)->format('Y-m-d 00:00:00');
+
+        $patientlast30  = Patient::whereBetween('created_at', [$day30, $today])->count();
+        $patientprev30  = Patient::whereBetween('created_at', [$d3060, $day30])->count();
+        $patientprev30  = ($patientprev30 == 0) ? 1 : $patientprev30;
+        $patientgrow30  = round((($patientlast30 - $patientprev30) / $patientprev30) * 100, 2);
+
+        $editlast30     = Patient::whereBetween('updated_at', [$day30, $today])->whereColumn('created_at', '!=', 'updated_at')->count();
+        $editprev30     = Patient::whereBetween('updated_at', [$d3060, $day30])->whereColumn('created_at', '!=', 'updated_at')->count();
+        $editprev30     = ($editprev30 == 0) ? 1 : $editprev30;
+        $editgrow30     = round((($editlast30 - $editprev30) / $editprev30) * 100, 2);
+
+        $patienttotals  = Patient::count();
+        $totalimages    = Persona::where('owner_type', 'patient')->whereNotNull('profile_photo')->count();
+
+        $stats = [
+            'patienttotals' => $patienttotals,
+            'patientlast30' => $patientlast30,
+            'patientgrow30' => $patientgrow30,
+            'editlast30'    => $editlast30,
+            'editgrow30'    => $editgrow30,
+            'totalimages'   => $totalimages,
+        ];
+
+        return view('Dashboard.index', compact('pageTitle', 'pageH2', 'stats'));
     }
 }
--- resources/CareUS/Dashboard/index.blade.php
+++ resources/CareUS/Dashboard/index.blade.php
@@ -7,7 +7,75 @@
     @section('content')
     <x-common.pageheader class="pt-10 pb-16">{{ $pageH2 }}</x-common.pageheader>
 
-    Content
+    <div class="flex flex-col items-center justify-center w-full md:space-x-10 md:flex-row">
+        <div class="flex flex-row w-3/12 p-5 rounded shadow-sm bg-burntsienna-100">
+            <div class="flex flex-col w-10/12">
+                <p class="text-sm uppercase text-burntsienna-300">{{ __('Total patients') }}</p>
+                <div class="flex flex-row items-center justify-center mt-2 text-center">
+                    <p class="w-4/12 text-3xl text-burntsienna-500">{{ $stats['patienttotals'] }}</p>
+                    <p class="w-4/12">&nbsp;</p>
+                    <p class="w-4/12">&nbsp;</p>
+                </div>
+            </div>
+            <i class="text-6xl text-burntsienna-200 fas fa-users"></i>
+        </div>
+
+        <div class="flex flex-row w-3/12 p-5 rounded shadow-sm bg-burntsienna-100">
+            <div class="flex flex-col w-10/12">
+                <p class="text-sm uppercase text-burntsienna-300">{{ __('New patients [last 30 days]') }}</p>
+                <div class="flex flex-row items-center justify-center mt-2 text-center">
+                    <p class="w-4/12 text-3xl text-burntsienna-500">{{ $stats['patientlast30'] }}</p>
+                    @if($stats['patientgrow30']<>0)
+                        @if($stats['patientgrow30']>0)
+                        <p class="w-3/12 py-1 text-xs text-green-800 bg-green-200">
+                            + {{ $stats['patientgrow30'] }} %
+                        </p>
+                        @else
+                        <p class="w-3/12 py-1 text-xs text-red-800 bg-red-200">
+                            {{ $stats['patientgrow30'] }} %
+                        </p>
+                        @endif
+                        @endif
+                        <p class="w-5/12">&nbsp;</p>
+                </div>
+            </div>
+            <i class="text-6xl text-burntsienna-200 fas fa-user-plus"></i>
+        </div>
+
+        <div class="flex flex-row w-3/12 p-5 rounded shadow-sm bg-burntsienna-100">
+            <div class="flex flex-col w-10/12">
+                <p class="text-sm uppercase text-burntsienna-300">{{ __('Edited patients [last 30 days]') }}</p>
+                <div class="flex flex-row items-center justify-center mt-2 text-center">
+                    <p class="w-4/12 text-3xl text-burntsienna-500">{{ $stats['editlast30'] }}</p>
+                    @if($stats['editgrow30']<>0)
+                        @if($stats['editgrow30']>0)
+                        <p class="w-3/12 py-1 text-xs text-green-800 bg-green-200">
+                            + {{ $stats['editgrow30'] }} %
+                        </p>
+                        @else
+                        <p class="w-3/12 py-1 text-xs text-red-800 bg-red-200">
+                            {{ $stats['editgrow30'] }} %
+                        </p>
+                        @endif
+                        @endif
+                        <p class="w-5/12">&nbsp;</p>
+                </div>
+            </div>
+            <i class="text-6xl text-burntsienna-200 fas fa-user-edit"></i>
+        </div>
+
+        <div class="flex flex-row w-3/12 p-5 rounded shadow-sm bg-burntsienna-100">
+            <div class="flex flex-col w-10/12">
+                <p class="text-sm uppercase text-burntsienna-300">{{ __('Patients photos') }}</p>
+                <div class="flex flex-row items-center justify-center mt-2 text-center">
+                    <p class="w-4/12 text-3xl text-burntsienna-500">{{ $stats['totalimages'] }}</p>
+                    <p class="w-4/12">&nbsp;</p>
+                    <p class="w-4/12">&nbsp;</p>
+                </div>
+            </div>
+            <i class="text-6xl text-burntsienna-200 fas fa-camera"></i>
+        </div>
+    </div>
     @endsection
 
     @push('scripts')
